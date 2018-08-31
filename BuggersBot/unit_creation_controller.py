2ca52e1ec722445c70a500d75df85d403a41bd2e
Nick H <manifes7o@gmail.com>
2018-08-31 15:13:18 -0400

    Add UnitCreationController

---
+++ BuggersBot/unit_creation_controller.py
@@ -0,0 +1,27 @@
+from sc2.constants import *
+
+
+class UnitCreationController:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def step(self):
+        await self.build_offensive_force()
+
+    async def build_offensive_force(self):
+        if self.bot.coordinator.check_unit_build(OVERLORD, supply_left_lt=5):
+            await self.bot.do(self.bot.coordinator.larvae.random.train(OVERLORD))
+
+        if self.bot.coordinator.check_unit_build(ZERGLING, max_units=50):
+            await self.bot.do(self.bot.coordinator.larvae.random.train(ZERGLING))
+
+        if self.bot.worker_controller.optimize_worker_ct():
+            await self.bot.do(self.bot.coordinator.larvae.random.train(DRONE))
+
+    def find_amass_army_rally_point(self):
+        """Figure out where to send new units when intentionally building an army"""
+        enemy_hq = self.bot.enemy_start_locations[0]
+        satellite = self.bot.townhalls.closest_to(enemy_hq)
+        point = satellite.position.towards(self.bot._game_info.map_center, distance=5, limit=True)
+        print("Setting Rally point to {}".format(point.position.to2))
+        return point
