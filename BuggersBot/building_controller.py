7a33024190554baff050131246ebef9b2c7a0de0
Nick H <manifes7o@gmail.com>
2018-08-31 18:22:17 -0400

    Add function to build vespene

--- BuggersBot/building_controller.py
+++ BuggersBot/building_controller.py
@@ -12,6 +12,17 @@ class BuildingController:
         self.bot = bot
 
     async def step(self):
-        pass
+        await self.expand_vespene()
 
+    async def expand_vespene(self):
+        bases = self.bot.globals.bases
+
+        for base in bases:
+            if base.assigned_harvesters < base.ideal_harvesters:
+                continue
+            vgs = self.bot.state.vespene_geyser.closer_than(15, base)
+            for vg in vgs:
+                if not self.bot.units(EXTRACTOR).closer_than(1, vg).exists:
+                    if self.bot.checker.building(EXTRACTOR):
+                        await self.bot.do(self.bot.units(DRONE).random.build(EXTRACTOR, vg))
 
