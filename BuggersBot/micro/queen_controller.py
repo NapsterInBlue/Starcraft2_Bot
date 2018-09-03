54b5e40ff15d02c941961cc58740898834fdd330
Nick H <manifes7o@gmail.com>
2018-09-03 19:07:42 -0400

    Remove redundant check

--- BuggersBot/micro/queen_controller.py
+++ BuggersBot/micro/queen_controller.py
@@ -6,11 +6,10 @@ class QueenController:
         self.bot = bot
 
     async def queen_behavior(self):
-        if self.bot.units(QUEEN).exists:
-            for queen in self.bot.units(QUEEN):
-                await self.bot.army_controller.defend(queen)
-                await self.bot.army_controller.patrol(queen, self.bot.townhalls)
+        for queen in self.bot.units(QUEEN):
+            await self.bot.army_controller.defend(queen)
+            await self.bot.army_controller.patrol(queen, self.bot.townhalls)
 
-                abilities = await self.bot.get_available_abilities(queen)
-                if EFFECT_INJECTLARVA in abilities:
-                    await self.bot.do(queen(EFFECT_INJECTLARVA, self.bot.townhalls.closest_to(queen)))
+            abilities = await self.bot.get_available_abilities(queen)
+            if EFFECT_INJECTLARVA in abilities:
+                await self.bot.do(queen(EFFECT_INJECTLARVA, self.bot.townhalls.closest_to(queen)))
